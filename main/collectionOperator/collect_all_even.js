735457aa3eb8bde915a0af4227dbcc6ba658ab8a
DoloresDai <841856753@qq.com>
2018-12-14 09:04:02 +0800

    Add collect all even of array

--- main/collectionOperator/collect_all_even.js
+++ main/collectionOperator/collect_all_even.js
@@ -1,7 +1,12 @@
 'use strict';
 
 function collect_all_even(collection) {
-  //在这里写入代码
+  var evenCollection = new Array;
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i] % 2 === 0) {
+      evenCollection.push(collection[i]);
+    }
+  }
+  return evenCollection;
 }
-
 module.exports = collect_all_even;
