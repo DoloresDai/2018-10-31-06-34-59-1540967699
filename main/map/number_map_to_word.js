33a1a1a0cc559d62defa78e59d3dccba8ee1b971
DoloresDai <841856753@qq.com>
2018-12-16 18:56:37 +0800

    Map number array to word array

--- main/map/number_map_to_word.js
+++ main/map/number_map_to_word.js
@@ -1,6 +1,5 @@
 'use strict';
-var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+var number_map_to_word = function(collectio) {
+  collection.map(x => String.fromCharCode(x + 96));
 };
-
 module.exports = number_map_to_word;
