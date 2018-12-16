d601ea2512d4b13f4984cf251e8075158092e9fd
DoloresDai <841856753@qq.com>
2018-12-16 19:12:36 +0800

    Get first even in an array

--- main/reduce/find_first_even.js
+++ main/reduce/find_first_even.js
@@ -1,8 +1,13 @@
 'use strict';
 
 function find_first_even(collection) {
-  //在这里写入代码
+  var index = 0;
+  var evenArray = [];
+  for (index in collection) {
+    if (collection[index] % 2 === 0) {
+      evenArray.push(collection[index]);
+    }
+  }
+  return evenArray[0];
 }
-
 module.exports = find_first_even;
-
