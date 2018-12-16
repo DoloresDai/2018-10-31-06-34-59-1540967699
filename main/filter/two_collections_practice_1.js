a6f7981e249f0a5f5126d22dfa6e1f93425aa62b
DoloresDai <841856753@qq.com>
2018-12-16 18:00:48 +0800

    Add getting common element from two array to form union array

--- main/filter/two_collections_practice_1.js
+++ main/filter/two_collections_practice_1.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function choose_common_elements(collection_a, collection_b) {
-
-  //在这里写入代码
+  var commonElement = [];
+  for (let i = 0; i < collection_a.length; i++) {
+    if (collection_b.includes(collection_a[i])) {
+      commonElement.push(collection_a[i]);
+    }
+  }
+  return commonElement;
 }
-
 module.exports = choose_common_elements;
