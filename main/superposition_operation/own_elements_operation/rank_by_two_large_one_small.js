0dcd101edf36ad3fa19f3918890dfc6d3cb9955b
DoloresDai <841856753@qq.com>
2018-12-16 20:07:26 +0800

    Get new array that parity is ranked by two large one small

--- main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,28 @@
 'use strict';
-function rank_by_two_large_one_small(collection){
-  //这里写代码。。。
+
+function rankDesc(collection) {
+  for (let i = 0; i < collection.length - 1; i++) {
+    for (let j = 0; j < collection.length - 1 - i; j++) {
+      if (collection[j] > collection[j + 1]) {
+        let temp = collection[j];
+        collection[j] = collection[j + 1];
+        collection[j + 1] = temp;
+      }
+    }
+  }
+  return collection;
+}
+
+function rank_by_two_large_one_small(collection) {
+  var descArray = rankDesc(collection);
+  var spliceTime = Math.ceil(collection.length / 3)
+  var spliceArray = [];
+  for (let i = 0; i < spliceTime; i += 2) {
+    spliceArray.push(descArray.splice(i, 1));
+  }
+  for (let j = 0; j < spliceArray.length; j++) {
+    descArray.splice(3 * j + 2, 0, spliceArray[j][0]);
+  }
+  return descArray;
 }
 module.exports = rank_by_two_large_one_small;
