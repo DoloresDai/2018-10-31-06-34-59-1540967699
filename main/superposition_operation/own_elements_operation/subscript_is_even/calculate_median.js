d4fa43302d98befb7640ca5e71996beb3fe18bdd
DoloresDai <841856753@qq.com>
2018-12-16 19:53:36 +0800

    Get median of even index elements in an array

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,5 +1,9 @@
 'use strict';
-var calculate_median = function(collection){
-
-};
+var calculate_median = function(collection) {
+  var evenIndexArray = [];
+  for (let i = 1; i < collection.length; i += 2) {
+    evenIndexArray.push(collection[i]);
+  }
+  return (evenIndexArray[Math.floor((evenIndexArray.length - 1) / 2)] + evenIndexArray[Math.ceil((evenIndexArray.length - 1) / 2)]) / 2
+}
 module.exports = calculate_median;
