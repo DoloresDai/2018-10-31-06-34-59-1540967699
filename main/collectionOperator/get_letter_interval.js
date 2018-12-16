93ccfb3d48b85df384efb2d7931d5da9549c702c
DoloresDai <841856753@qq.com>
2018-12-16 17:37:03 +0800

    Add getting single letter interval according to numbers

--- main/collectionOperator/get_letter_interval.js
+++ main/collectionOperator/get_letter_interval.js
@@ -1,7 +1,29 @@
 'use strict';
 
-function get_letter_interval(number_a, number_b) {
-  //在这里写入代码
+function getCharCode(number_a, number_b) {
+  var Array = [number_a, number_b]
+  var string = Array.join();
+  number_a = string.charCodeAt(0);
+  number_b = string.charCodeAt(1);
+  return number_a, number_b;
 }
 
+function getIntegerArray(number_a, number_b) {
+  let varriable = number_b - number_a;
+  var intervalArray = new Array；
+  while (number_b >= varriable) {
+    varriable = number_a++;
+    intervalArray.push(varriable);
+  }
+  return intervalArray;
+}
+
+function get_letter_interval(number_a, number_b) {
+  let varriable = number_b - number_a;
+  var charCodeArray = [];
+  for (let i = 0; i < varriable; i++) {
+    charCodeArray.push(intervalArray.join.charCodeArray(i));
+  }
+  return charCodeArray;
+}
 module.exports = get_letter_interval;
