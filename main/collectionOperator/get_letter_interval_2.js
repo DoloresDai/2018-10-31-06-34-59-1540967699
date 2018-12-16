cf834218b0409a1dcdd03485313f772a5ec9ccb1
DoloresDai <841856753@qq.com>
2018-12-16 17:38:02 +0800

    Add geting double letter interval according to numbers

--- main/collectionOperator/get_letter_interval_2.js
+++ main/collectionOperator/get_letter_interval_2.js
@@ -1,8 +1,32 @@
 'use strict';
 
-function get_letter_interval_2(number_a, number_b) {
-  //在这里写入代码
+function getCharCode(number) {
+  if (number < 27) {
+    return String.fromCharCode(number + 96);
+  } else if (number < 53 && number > 26) {
+    return 'a' + String.fromCharCode(number + 70);
+  } else {
+    return 'b' + String.fromCharCode(number + 44);
+  }
 }
 
-module.exports = get_letter_interval_2;
+function getCharCodeArray(number_a, number_b) {
+  var getLetterInterval = [];
+  for (number_a; number_a <= number_b; number_a++) {
+    if (number_a < 27) {
+      getLetterInterval.push(getCharCode(number_a))
+    } else {
+      getLetterInterval.push(getCharCode(number_a))
+    }
+  }
+  return getLetterInterval;
+}
 
+function get_letter_interval_2(number_a, number_b) {
+  if (number_a > number_b) {
+    return getCharCodeArray(number_b, number_a).reverse();
+  } else {
+    return getCharCodeArray(number_a, number_b)
+  }
+}
+module.exports = get_letter_interval_2;
