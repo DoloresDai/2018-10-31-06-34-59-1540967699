7cee9d929c386c60def87fe279f386f6803d8c46
DoloresDai <841856753@qq.com>
2018-12-16 17:59:33 +0800

    Add getting groups of counts

--- main/filter/grouping_count.js
+++ main/filter/grouping_count.js
@@ -1,8 +1,27 @@
 'use strict';
 
-function grouping_count(collection) {
-
-  //在这里写入代码
+function groupingCountObj(key, val) {
+  var groupingCount = {};
+  groupingCount[key] = val;
+  return groupingCount;
 }
 
+function grouping_count(collection) {
+  var noRepeatArray = [];
+  var counts = [];
+  for (var i = 0; i < collection.length;) {
+    var count = 0;
+    for (let j = i; j < collection.length; j++) {
+      if (collection[i] == collection[j]) {
+        count++;
+      }
+    }
+    noRepeatArray.push(collection[i]);
+    counts.push(count);
+    i += count;
+  }
+  for (let k = 0; k < noRepeatArray.length; k++) {
+    groupingCountObj(noRepeatArray[k], counts[k]);
+  }
+}
 module.exports = grouping_count;
