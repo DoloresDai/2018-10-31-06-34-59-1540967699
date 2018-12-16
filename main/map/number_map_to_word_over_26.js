27c62a95b4a20170a957989229805ca18f9d15cf
DoloresDai <841856753@qq.com>
2018-12-16 18:55:55 +0800

    Map number array to word array over 26

--- main/map/number_map_to_word_over_26.js
+++ main/map/number_map_to_word_over_26.js
@@ -1,6 +1,13 @@
 'use strict';
-var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
-};
-
+var number_map_to_word_over_26 = function(collection) {
+  var codeNumber = collection.map(x => x + 96);
+  for (let i = 0; i < codeNumber.length; i++) {
+    if (codeNumber[i] < 123) {
+      codeNumber[i] = String.fromCharCode(codeNumber[i]);
+    } else {
+      codeNumber[i] = 'a' + String.fromCharCode(codeNumber[i] - 26);
+    }
+  }
+  return codeNumber;
+}
 module.exports = number_map_to_word_over_26;
