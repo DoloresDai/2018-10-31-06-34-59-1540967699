61d597f5e76bb3408f52a47a06e922455f05f12f
DoloresDai <841856753@qq.com>
2018-12-16 20:04:37 +0800

    Get new array that parity is divided into two ends depending on rise and fall

--- main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,39 @@
 'use strict';
-var even_asc_odd_desc = function(collection){
 
+function getEvenArray(collection) {
+  return collection.filter(x => x % 2 === 0);
+}
+
+function getOddArray(collection) {
+  return collection.filter(x => x % 2 != 0);
+}
+
+function rankDesc(collection) {
+  for (let i = 0; i < collection.length - 1; i++) {
+    for (let j = 0; j < collection.length - 1 - i; j++) {
+      if (collection[j] > collection[j + 1]) {
+        let temp = collection[j];
+        collection[j] = collection[j + 1];
+        collection[j + 1] = temp;
+      }
+    }
+  }
+  return collection;
+}
+
+function rankAsc(collection) {
+  for (let i = 0; i < collection.length - 1; i++) {
+    for (let j = 0; j < collection.length - 1 - i; j++) {
+      if (collection[j] < collection[j + 1]) {
+        let temp = collection[j];
+        collection[j] = collection[j + 1];
+        collection[j + 1] = temp;
+      }
+    }
+  }
+  return collection;
+}
+var even_asc_odd_desc = function(collection) {
+  return rankDesc(getEvenArray(collection)).concat(rankAsc(getOddArray(collection)));
 };
 module.exports = even_asc_odd_desc;
