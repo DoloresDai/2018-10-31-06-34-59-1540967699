619ad3700cebbfc21bc360e119ff5e782f60eae1
DoloresDai <841856753@qq.com>
2018-12-16 19:32:01 +0800

    Map average of array to letter

--- main/superposition_operation/collection_operation/letter_mapping_2.js
+++ main/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,9 +1,6 @@
 'use strict';
 
 function average_to_letter(collection) {
-
-  //在这里写入代码
+  return String.fromCharCode(Math.ceil(collection.reduce((x, y) => x + y) / collection.length + 96));
 }
-
 module.exports = average_to_letter;
-
