5d441e96881bc4f506d9c998e4a761146065ea85
DoloresDai <841856753@qq.com>
2018-12-16 18:02:02 +0800

    Add getting uncommon elements from two array to form new array

--- main/filter/two_collections_practice_2.js
+++ main/filter/two_collections_practice_2.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function choose_no_common_elements(collection_a, collection_b) {
-
-  //在这里写入代码
+  var noCommonElement = [];
+  for (let i = 0; i < collection_a.length; i++) {
+    if (!collection_b.includes(collection_a[i])) {
+      noCommonElement.push(collection_a[i]);
+    }
+  }
+  return noCommonElement;
 }
-
 module.exports = choose_no_common_elements;
