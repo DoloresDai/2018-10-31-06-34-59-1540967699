ba534f9426dfdd1edeb2a654c9143b6dacec246a
DoloresDai <841856753@qq.com>
2018-12-16 19:25:23 +0800

    Get alphabetic elemeents intersection in twoobjects

--- main/section_1/practice_4/practice.js
+++ main/section_1/practice_4/practice.js
@@ -1,5 +1,11 @@
 function collect_same_elements(collection_a, object_b) {
-  //在这里写入代码
+  var commonElement = [];
+  var index = 0;
+  for (index in collection_a) {
+    if (object_b.value.includes(Object.values(collection_a[index])[0])) {
+      commonElement.push(Object.values(collection_a[index])[0]);
+    }
+  }
+  return commonElement;
 }
-
 module.exports = collect_same_elements;
