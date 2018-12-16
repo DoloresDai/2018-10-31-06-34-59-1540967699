468f151b0207ab2ed89081db2f6c210c1fe58ee7
DoloresDai <841856753@qq.com>
2018-12-16 19:01:21 +0800

    Collect max number in an array

--- main/reduce/collect_max_number.js
+++ main/reduce/collect_max_number.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function collect_max_number(collection) {
-  //在这里写入代码
+  var maxNumber = collection.reduce(function(x, y) {
+    return x > y ? x : y
+  });
 }
-
 module.exports = collect_max_number;
