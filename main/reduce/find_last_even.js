5d80d05d24b6e71c64d4c250f407dfa52d20362b
DoloresDai <841856753@qq.com>
2018-12-16 19:13:30 +0800

    Get last even element in an array

--- main/reduce/find_last_even.js
+++ main/reduce/find_last_even.js
@@ -1,7 +1,13 @@
 'use strict';
 
 function find_last_even(collection) {
-  //在这里写入代码
+  var index = 0;
+  var evenArray = [];
+  for (index in collection) {
+    if (collection[index] % 2 === 0) {
+      evenArray.push(collection[index]);
+    }
+  }
+  return evenArray[evenArray.length - 1];
 }
-
 module.exports = find_last_even;
