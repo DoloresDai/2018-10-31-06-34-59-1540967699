4e84801bc073cad1d0ae79e444909d52327c4805
DoloresDai <841856753@qq.com>
2018-12-16 19:56:36 +0800

    Get average of even index elements according several digits

--- main/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
@@ -1,5 +1,19 @@
 'use strict';
-var even_group_calculate_average = function(collection){
-
+var even_group_calculate_average = function(collection) {
+  var evenIndexArray = [];
+  var averageArray = [];
+  for (let i = 1; i < collection.length; i += 2) {
+    evenIndexArray.push(collection[i]);
+  }
+  var evenArray = evenIndexArray.filter(x => x % 2 === 0);
+  if (evenArray.length === 0) {
+    console.log([0]);
+  } else {
+    let oneDigitAverage = evenArray.filter(x => x < 10 && x >= 0).reduce((x, y) => x + y) / evenArray.filter(x => x < 10 && x >= 0).length;
+    let twoDigitAverage = evenArray.filter(x => x < 100 && x >= 10).reduce((x, y) => x + y) / evenArray.filter(x => x < 100 && x >= 10).length;
+    let threeDigitAverage = evenArray.filter(x => x < 1000 && x >= 100).reduce((x, y) => x + y) / evenArray.filter(x => x < 1000 && x >= 100).length;
+    averageArray.push(oneDigitAverage, twoDigitAverage, threeDigitAverage);
+  }
+  return averageArray;
 };
 module.exports = even_group_calculate_average;
