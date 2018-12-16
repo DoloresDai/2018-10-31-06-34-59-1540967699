8dfc568cf3bca89a1265c31d2c1be3998f9045b2
DoloresDai <841856753@qq.com>
2018-12-16 20:00:45 +0800

    Get uncommon elements in even order

--- main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,24 @@
 'use strict';
-var single_element = function(collection){
 
+function getEvenSequenceArray(collection) {
+  var evenSequenceArray = [];
+  for (let i = 1; i < collection.length; i += 2) {
+    evenSequenceArray.push(collection[i]);
+  }
+  return evenSequenceArray;
+}
+
+function chooseNoRepeatNumber(collection) {
+  var noRepeatArray = [];
+  var index = 0;
+  for (index in collection) {
+    if (collection.indexOf(collection[index]) === collection.lastIndexOf(collection[index])) {
+      noRepeatArray.push(collection[index]);
+    }
+  }
+  return noRepeatArray;
+}
+var single_element = function(collection) {
+  return chooseNoRepeatNumber(getEvenSequenceArray(collection));
 };
 module.exports = single_element;
