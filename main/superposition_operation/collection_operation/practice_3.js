a16fa26eb878ebf795bb32c8f24bfb17a6361a97
DoloresDai <841856753@qq.com>
2018-12-16 19:45:03 +0800

    Map odd numbers to three multiples add five and sum all

--- main/superposition_operation/collection_operation/practice_3.js
+++ main/superposition_operation/collection_operation/practice_3.js
@@ -1,9 +1,6 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
-
-  //在这里写入代码
+  return collection.filter(x => x % 2 != 0).map(x => x * 3 + 5).reduce((x, y) => x + y);
 }
-
 module.exports = hybrid_operation_to_uneven;
-
