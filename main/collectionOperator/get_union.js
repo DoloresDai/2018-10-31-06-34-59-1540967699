90680d24bb183b071892a556c21e5baf88f4cfa2
DoloresDai <841856753@qq.com>
2018-12-16 17:39:06 +0800

    Add getting union array

--- main/collectionOperator/get_union.js
+++ main/collectionOperator/get_union.js
@@ -1,8 +1,20 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-  //在这里写入代码
+  if (collection_a.length - collection_b.length > 0) {
+    for (let i = 0; i < collection_a.length; i++) {
+      if (!collection_b.includes(collection_a[i])) {
+        collection_b.push(collection_a[i]);
+      }
+    }
+    return collection_b;
+  } else {
+    for (let i = 0; i < collection_b.length; i++) {
+      if (!collection_a.includes(collection_b[i])) {
+        collection_a.push(collection_b[i]);
+      }
+    }
+  }
+  return collection_a;
 }
-
 module.exports = get_union;
-
