e41683f714adbf8c1cd54d140ddbe0d6dfd96a0a
DoloresDai <841856753@qq.com>
2018-12-16 19:51:35 +0800

    Average even index elements

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,15 @@
 'use strict';
-var calculate_average = function(collection){
-
-};
+var calculate_average = function(collection) {
+  var index = 0;
+  var evenIndexArray = [];
+  // for (index in collection) {
+  //   if ((index + 1) % 2 === 0) {
+  //     evenIndexArray.push(collection[index]);
+  //   }
+  // }
+  for (let i = 1; i < collection.length; i += 2) {
+    evenIndexArray.push(collection[i]);
+  }
+  return evenIndexArray.reduce((x, y) => x + y) / evenIndexArray.length;
+}
 module.exports = calculate_average;
