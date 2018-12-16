6ef32044c8692570c813824057397af6ad3b31b9
DoloresDai <841856753@qq.com>
2018-12-16 19:02:05 +0800

    Collect min number in an array

--- main/reduce/collect_min_number.js
+++ main/reduce/collect_min_number.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function collect_min_number(collection) {
-  //在这里写入代码
+  var mimNumber = collection.reduce(function(x, y) {
+    return x < y ? x : y
+  });
 }
-
 module.exports = collect_min_number;
-
