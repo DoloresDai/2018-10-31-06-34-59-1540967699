9ac2e242469ad9f165c802aa14378b27217177e8
DoloresDai <841856753@qq.com>
2018-12-16 17:56:39 +0800

    Add choosing multiples of three

--- main/filter/choose_multiples_of_three.js
+++ main/filter/choose_multiples_of_three.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function choose_multiples_of_three(collection) {
-
-  //在这里写入代码
+  var multipleOfThree = [];
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i] % 3 === 0) {
+      multipleOfThree.push(collection[i]);
+    }
+  }
+  return multipleOfThree;
 }
-
 module.exports = choose_multiples_of_three;
