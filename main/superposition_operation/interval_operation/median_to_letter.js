3aca3ebc602d9854c722e2023aa67fe870061aeb
DoloresDai <841856753@qq.com>
2018-12-16 19:49:56 +0800

    Map median to letter

--- main/superposition_operation/interval_operation/median_to_letter.js
+++ main/superposition_operation/interval_operation/median_to_letter.js
@@ -1,8 +1,6 @@
 'use strict';
 
 function median_to_letter(collection) {
-
-  //在这里写入代码
+  return 'a' + String.fromCharCode(Math.ceil((collection[(53 - 20 + 1) / 2 - 1] + collection[(53 - 20 + 1) / 2]) / 2) + 96 - 26);
 }
-
 module.exports = median_to_letter;
