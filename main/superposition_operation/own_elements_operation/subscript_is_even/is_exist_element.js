89e2e9be4158b24e1b22b7b03c3951f5752b8fb0
DoloresDai <841856753@qq.com>
2018-12-16 19:59:26 +0800

    Judge existence of elements in even index

--- main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,14 @@
 'use strict';
-var is_exist_element = function(collection,element){
 
+function getEvenIndexArray(collection) {
+  var evenIndexArray = [];
+  for (let i = 0; i < collection.length; i += 2) {
+    evenIndexArray.push(collection[i]);
+  }
+  return evenIndexArray;
+}
+var is_exist_element = function(collection, element) {
+  return getEvenIndexArray(collection).includes(element);
+  // return getEvenIndexArray(collection).some(x => x === element)
 };
 module.exports = is_exist_element;
