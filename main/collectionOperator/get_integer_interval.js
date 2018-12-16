a9495041242365bf72b94c690dd59f45ee9337d4
DoloresDai <841856753@qq.com>
2018-12-16 17:28:41 +0800

    Add getting integer's interval

--- main/collectionOperator/get_integer_interval.js
+++ main/collectionOperator/get_integer_interval.js
@@ -1,8 +1,25 @@
 'use strict';
 
 function get_integer_interval(number_a, number_b) {
-  //在这里写入代码
+  let difference = number_b - number_a;
+  var newArray = [];
+  if (number_b - number_a > 0) {
+    return newArray = getArray
+  } else if (number_b - number_a === 0) {
+    return newArray.push(number_a)
+  } else if (number_b - number_a < 0) {
+    return newArray = getArray.reverse;
+  }
 }
+return newArray;
 
+function getArray(number_a, number_b) {
+  let varriable;
+  var intervalArray = new Array；
+  while (number_b >= varriable) {
+    varriable = number_a++;
+    intervalArray.push(varriable);
+  }
+  return intervalArray;
+}
 module.exports = get_integer_interval;
-
