4f3c99a01c30e397d386cbbf6c99725fe36b71f0
DoloresDai <841856753@qq.com>
2018-12-16 17:55:23 +0800

    Add choosing even from array

--- main/filter/choose_even.js
+++ main/filter/choose_even.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function choose_even(collection) {
-
-  //在这里写入代码
+  var evenCollection = new Array;
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i] % 2 === 0) {
+      evenCollection.push(collection[i]);
+    }
+  }
+  return evenCollection;
 }
-
 module.exports = choose_even;
