7821e4544d66c2ad18f204ed200e0b5afc79dceb
DoloresDai <841856753@qq.com>
2018-12-16 19:08:12 +0800

    Compute median of even index elements in arrays

--- main/reduce/compute_median.js
+++ main/reduce/compute_median.js
@@ -1,9 +1,12 @@
 'use strict';
 
 function compute_median(collection) {
-  //在这里写入代码
+  var len = collection.length;
+  var medium = Math.floor((len - 1) / 2);
+  if (len % 2 != 0) {
+    return collection[medium]
+  } else {
+    return (collection[medium] + collection[medium + 1]) / 2;
+  }
 }
-
 module.exports = compute_median;
-
-
