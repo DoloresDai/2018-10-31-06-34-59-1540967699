489496f42ad3cd537d0b82ecb9425a935304aea4
DoloresDai <841856753@qq.com>
2018-12-16 19:30:07 +0800

    Map even number to letter

--- main/superposition_operation/collection_operation/letter_mapping_1.js
+++ main/superposition_operation/collection_operation/letter_mapping_1.js
@@ -1,8 +1,8 @@
 'use strict';
 
+function getEvenArray(collection) {}
+
 function even_to_letter(collection) {
-
-  //在这里写入代码
+  return collection.filter(x => x % 2 === 0).map(x => String.fromCharCode(x + 96));
 }
-
 module.exports = even_to_letter;
