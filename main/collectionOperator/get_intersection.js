296089a05061eedb71c097dedfee5cb0ea2015e6
DoloresDai <841856753@qq.com>
2018-12-16 17:34:15 +0800

    Add getting intersection

--- main/collectionOperator/get_intersection.js
+++ main/collectionOperator/get_intersection.js
@@ -1,7 +1,14 @@
 'use strict';
 
 function get_intersection(collection_a, collection_b) {
-  //在这里写入代码
+  var intersectionArray = [];
+  for (let i = 0; i < collection_a.length; i++) {
+    for (let j = 0; j < collection_b.length; j++) {
+      if (collection_a[i] = collection_b[j]) {
+        intersectionArray.push(collection_a[i]);
+      }
+    }
+    return intersectionArray;
+  }
 }
-
 module.exports = get_intersection;
