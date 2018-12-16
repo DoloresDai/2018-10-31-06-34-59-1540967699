456f6ddae3a16ecd6e9c7d36dc97811e9e725981
DoloresDai <841856753@qq.com>
2018-12-16 18:45:23 +0800

    Convert two-dimensional array to ordered no repeat one-dimensional array

--- main/flaten/double_dimensional_array_to_one_2.js
+++ main/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,17 @@
 'use strict';
 
-function double_to_one(collection) {
-
-  //在这里写入代码
+function choose_no_repeat_number(collection) {
+  var noRepeatArray = [];
+  for (let i = 0; i < collection.length; i++) {
+    if (!noRepeatArray.includes(collection[i])) {
+      noRepeatArray.push(collection[i]);
+    }
+  }
+  return noRepeatArray;
 }
 
+function double_to_one(collection) {
+  var getArray = [].concat.apply([], collection);
+  return choose_no_repeat_number(getArray);
+}
 module.exports = double_to_one;
