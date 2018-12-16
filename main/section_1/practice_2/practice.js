121fda8640be559d144eb1e3cf9ff8bc62356639
DoloresDai <841856753@qq.com>
2018-12-16 19:23:28 +0800

    Get alphabetic elemeents in child-array

--- main/section_1/practice_2/practice.js
+++ main/section_1/practice_2/practice.js
@@ -1,5 +1,11 @@
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+  var commonElement = [];
+  var index = 0;
+  for (index in collection_a) {
+    if (collection_b[0].includes(collection_a[index])) {
+      commonElement.push(collection_a[index]);
+    }
+  }
+  return commonElement;
 }
-
 module.exports = collect_same_elements;
