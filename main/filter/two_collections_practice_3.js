19384c136ef762a973be703097ea330a9a69dd50
DoloresDai <841856753@qq.com>
2018-12-16 18:09:05 +0800

    Add getting numbers in A array that can be divisible by B array

--- main/filter/two_collections_practice_3.js
+++ main/filter/two_collections_practice_3.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
-
-  //在这里写入代码
+  var divisibleInteger = [];
+  for (let i = 0; i < collection_a.length; i++) {
+    for (let j = 0; j < collection_b.length; j++) {
+      if (collection_a[i] % collection_b[j] == 0) {
+        divisibleInteger.push(collection_a[i]);
+      }
+    }
+  }
+  return divisibleInteger;
 }
-
 module.exports = choose_divisible_integer;
