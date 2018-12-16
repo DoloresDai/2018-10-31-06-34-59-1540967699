7479041c8203e66c2600a964f6f521c4996dc53b
DoloresDai <841856753@qq.com>
2018-12-16 19:17:19 +0800

    Split number to zero

--- main/ruduce/split_to_zero.js
+++ main/ruduce/split_to_zero.js
@@ -1,7 +1,11 @@
 'use strict';
 
 function spilt_to_zero(number, interval) {
-  //在这里写入代码
+  var ruduceArray = [];
+  while (number > 0) {
+    ruduceArray.push(number);
+    number = number - interval;
+  }
+  return ruduceArray;
 }
-
 module.exports = spilt_to_zero;
