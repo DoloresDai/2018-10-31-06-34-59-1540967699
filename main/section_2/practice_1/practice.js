d7f50dd41be758a1e755228ddc8d6e3d83a4dfed
DoloresDai <841856753@qq.com>
2018-12-17 17:25:03 +0800

    Group array in form of key and count

--- main/section_2/practice_1/practice.js
+++ main/section_2/practice_1/practice.js
@@ -1,5 +1,35 @@
-function count_same_elements(collection) {
-  //在这里写入代码
+'use strict';
+
+function groupingCountObj(key, val) {
+  var groupingCount = {
+    'key': key,
+    'count': val
+  };
+  return groupingCount;
 }
 
+function count_same_elements(collection) {
+  var count = 0;
+  var counts = [];
+  var noRepeatArray = [];
+  var groupingArray = [];
+  for (let i = 0; i < collection.length; i++) {
+    if (!noRepeatArray.includes(collection[i])) {
+      noRepeatArray.push(collection[i]);
+    }
+  }
+  for (let j = 0; j < noRepeatArray.length; j++) {
+    for (let k = 0; k < collection.length; k++) {
+      if (noRepeatArray[j].includes(collection[k])) {
+        count++;
+      }
+    }
+    counts.push(count);
+    count = 0;
+  }
+  for (let index in noRepeatArray) {
+    groupingArray.push(groupingCountObj(noRepeatArray[index], counts[index]));
+  }
+  return groupingArray;
+}
 module.exports = count_same_elements;
