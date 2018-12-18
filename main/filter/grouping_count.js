e003fd24e07199273b801af039cdf1d83900c41b
DoloresDai <841856753@qq.com>
2018-12-18 10:41:44 +0800

    Modify code for pass the test

--- main/filter/grouping_count.js
+++ main/filter/grouping_count.js
@@ -1,27 +1,28 @@
 'use strict';
 
-function groupingCountObj(key, val) {
-  var groupingCount = {};
-  groupingCount[key] = val;
-  return groupingCount;
-}
-
 function grouping_count(collection) {
-  var noRepeatArray = [];
+  var count = 0;
   var counts = [];
-  for (var i = 0; i < collection.length;) {
-    var count = 0;
-    for (let j = i; j < collection.length; j++) {
-      if (collection[i] == collection[j]) {
+  var noRepeatArray = [];
+  var groupingArray = [];
+  var groupingCount = {};
+  for (let i = 0; i < collection.length; i++) {
+    if (!noRepeatArray.includes(collection[i])) {
+      noRepeatArray.push(collection[i]);
+    }
+  }
+  for (let j = 0; j < noRepeatArray.length; j++) {
+    for (let k = 0; k < collection.length; k++) {
+      if (noRepeatArray[j] === (collection[k])) {
         count++;
       }
     }
-    noRepeatArray.push(collection[i]);
     counts.push(count);
-    i += count;
+    count = 0;
   }
-  for (let k = 0; k < noRepeatArray.length; k++) {
-    groupingCountObj(noRepeatArray[k], counts[k]);
+  for (let index in noRepeatArray) {
+    groupingCount[noRepeatArray[index]] = counts[index];
   }
+  return groupingCount;
 }
 module.exports = grouping_count;
