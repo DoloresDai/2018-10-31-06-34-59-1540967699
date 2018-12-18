4c0ffacfd72c1e0f2c386d48f9892a88a9716e08
DoloresDai <841856753@qq.com>
2018-12-19 02:43:06 +0800

    Restructure code but fail for test becase unexpected token

--- main/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
@@ -1,19 +1,34 @@
 'use strict';
-var even_group_calculate_average = function(collection) {
-  var evenIndexArray = [];
-  var averageArray = [];
-  for (let i = 1; i < collection.length; i += 2) {
-    evenIndexArray.push(collection[i]);
+
+function getDigitArray(evenArray) {
+  for (let i = 0; i < intervalArray.length; i++) {
+    if (intervalArray[i] < 10 && intervalArray[i] >= 0) {
+      var oneDigitArray.push(intervalArray[i]);
+    } else if (intervalArray[i] < 100 && intervalArray[i] >= 10) {
+      var twoDigitArray.push(intervalArray[i]);
+    } else if (intervalArray[i] < 1000 && intervalArray[i] >= 100) {
+      var threeDigitArray.push(intervalArray[i]);
+    }
   }
-  var evenArray = evenIndexArray.filter(x => x % 2 === 0);
-  if (evenArray.length === 0) {
-    console.log([0]);
-  } else {
-    let oneDigitAverage = evenArray.filter(x => x < 10 && x >= 0).reduce((x, y) => x + y) / evenArray.filter(x => x < 10 && x >= 0).length;
-    let twoDigitAverage = evenArray.filter(x => x < 100 && x >= 10).reduce((x, y) => x + y) / evenArray.filter(x => x < 100 && x >= 10).length;
-    let threeDigitAverage = evenArray.filter(x => x < 1000 && x >= 100).reduce((x, y) => x + y) / evenArray.filter(x => x < 1000 && x >= 100).length;
-    averageArray.push(oneDigitAverage, twoDigitAverage, threeDigitAverage);
+
+  function countAverage(digitArray) {
+    return digitArray.reduce((x, y) => x + y) / digitArray.length;
   }
-  return averageArray;
-};
-module.exports = even_group_calculate_average;
+  var even_group_calculate_average = function(collection) {
+    var evenIndexArray = [];
+    var averageArray = [];
+    for (let i = 1; i < collection.length; i += 2) {
+      evenIndexArray.push(collection[i]);
+    }
+    var evenArray = evenIndexArray.filter(x => x % 2 === 0);
+    if (evenArray.length === 0) {
+      return [0];
+    } else {
+      let oneDigitAverage = countAverage(oneDigitAverage);
+      let twoDigitAverage = countAverage(twoDigitArray);
+      let threeDigitAverage = countAverage(threeDigitArray);
+      averageArray.push(oneDigitAverage, twoDigitAverage, threeDigitAverage);
+    }
+    return averageArray;
+  };
+  module.exports = even_group_calculate_average;
