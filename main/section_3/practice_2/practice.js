fd600e78951c7dcc24792a4cebdb8e13a2cc336b
DoloresDai <841856753@qq.com>
2018-12-18 11:43:07 +0800

    Match the attributes and B objects of A array to output a new A array that subtract 1 for every 3

--- main/section_3/practice_2/practice.js
+++ main/section_3/practice_2/practice.js
@@ -1,5 +1,9 @@
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  for (let j = 0; j < collection_a.length; j++) {
+    if (object_b.value.includes(collection_a[j].key)) {
+      collection_a[j].count = collection_a[j].count - Math.floor(collection_a[j].count / 3);
+    }
+  }
+  return collection_a;
 }
-
 module.exports = create_updated_collection;
