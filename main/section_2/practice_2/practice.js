b5dbaaca568d063ee45c499e0e76d1cb1e82d650
DoloresDai <841856753@qq.com>
2018-12-18 15:15:40 +0800

    Count non-character array

--- main/section_2/practice_2/practice.js
+++ main/section_2/practice_2/practice.js
@@ -1,5 +1,45 @@
-function count_same_elements(collection) {
-  //在这里写入代码
+function groupingCountObj(key, val) {
+  var groupingCount = {
+    'key': key,
+    'count': val
+  };
+  return groupingCount;
 }
 
-module.exports = count_same_elements;
+function convertNumberToLetter(collection) {
+  for (let n = 0; n < collection.length; n++) {
+    if (/[0-9]/.test(collection[n])) {
+      for (let d = 0; d < collection[n].replace(/[^a-z]/, "").replace(/[a-z]/, ""); d++) {
+        collection.splice(n + d, 0, collection[n].match(/[a-z]/g)[0]);
+      }
+    }
+  }
+  return collection;
+}
+
+function count_same_elements(collection) {
+  var count = 0;
+  var counts = [];
+  var noRepeatArray = [];
+  var groupingArray = [];
+  collection = convertNumberToLetter(collection);
+  for (let i = 0; i < collection.length; i++) {
+    if (!noRepeatArray.includes(collection[i])) {
+      noRepeatArray.push(collection[i].match(/[a-z]/ig)[0]);
+    }
+  }
+  for (let j = 0; j < noRepeatArray.length; j++) {
+    for (let k = 0; k < collection.length; k++) {
+      if (noRepeatArray[j] === (collection[k])) {
+        count++;
+      }
+    }
+    counts.push(count);
+    count = 0;
+  }
+  for (let index in noRepeatArray) {
+    groupingArray.push(groupingCountObj(noRepeatArray[index], counts[index]));
+  }
+  return groupingArray;
+}
+module.exports = convertNumberToLetter;
