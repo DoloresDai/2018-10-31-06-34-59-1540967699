302dd012649db706c4c0dd3b06ce3e2f8e361e12
DoloresDai <841856753@qq.com>
2018-12-19 00:26:50 +0800

    Modify code for pass the test

--- main/collectionOperator/get_intersection.js
+++ main/collectionOperator/get_intersection.js
@@ -2,13 +2,11 @@
 
 function get_intersection(collection_a, collection_b) {
   var intersectionArray = [];
-  for (let i = 0; i < collection_a.length; i++) {
-    for (let j = 0; j < collection_b.length; j++) {
-      if (collection_a[i] = collection_b[j]) {
-        intersectionArray.push(collection_a[i]);
-      }
+  for (let i = 0; i < collection_b.length; i++) {
+    if (collection_a.includes(collection_b[i])) {
+      intersectionArray.push(collection_b[i]);
     }
-    return intersectionArray;
   }
+  return intersectionArray;
 }
 module.exports = get_intersection;
