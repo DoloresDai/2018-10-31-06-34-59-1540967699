b9ace98bf846736b271c5e86b2b059498355b8b5
DoloresDai <841856753@qq.com>
2018-12-18 11:31:43 +0800

    Modify code format

--- main/section_3/practice_1/practice.js
+++ main/section_3/practice_1/practice.js
@@ -1,7 +1,7 @@
 function create_updated_collection(collection_a, object_b) {
   for (let j = 0; j < collection_a.length; j++) {
     if (object_b.value.includes(collection_a[j].key)) {
-      collection_a[j].count -= 1
+      collection_a[j].count -= 1;
     }
   }
   return collection_a;
