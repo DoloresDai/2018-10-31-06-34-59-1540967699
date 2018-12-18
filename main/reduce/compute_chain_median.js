1e34219cbfde54d685f115103b2c47babeed3d43
DoloresDai <841856753@qq.com>
2018-12-19 03:32:22 +0800

    Count median of chain

--- main/reduce/compute_chain_median.js
+++ main/reduce/compute_chain_median.js
@@ -1,7 +1,9 @@
 'use strict';
 
-function compute_chain_median(collection) {
-  //在这里写入代码
+function compute_chain_median(chain) {
+  var sortArray = chain.split('->').sort((x, y) => x - y);
+  var floor = Math.floor((sortArray.length - 1) / 2);
+  var ceil = Math.ceil((sortArray.length - 1) / 2);
+  return (Number(sortArray[floor]) + Number(sortArray[ceil])) / 2;
 }
-
 module.exports = compute_chain_median;
