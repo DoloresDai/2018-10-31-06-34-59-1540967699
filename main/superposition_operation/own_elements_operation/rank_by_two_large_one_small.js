c27fcde69a799090d901940875bad7b79b3e380b
DoloresDai <841856753@qq.com>
2018-12-19 14:41:49 +0800

    Rename function

--- main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function rankDesc(collection) {
+function rankAsc(collection) {
   for (let i = 0; i < collection.length - 1; i++) {
     for (let j = 0; j < collection.length - 1 - i; j++) {
       if (collection[j] > collection[j + 1]) {
@@ -14,15 +14,15 @@ function rankDesc(collection) {
 }
 
 function rank_by_two_large_one_small(collection) {
-  var descArray = rankDesc(collection);
+  var ascArray = rankAsc(collection);
   var spliceTime = Math.ceil(collection.length / 3)
   var spliceArray = [];
   for (let i = 0; i < spliceTime; i += 2) {
-    spliceArray.push(descArray.splice(i, 1));
+    spliceArray.push(ascArray.splice(i, 1));
   }
   for (let j = 0; j < spliceArray.length; j++) {
-    descArray.splice(3 * j + 2, 0, spliceArray[j][0]);
+    ascArray.splice(3 * j + 2, 0, spliceArray[j][0]);
   }
-  return descArray;
+  return ascArray;
 }
 module.exports = rank_by_two_large_one_small;
