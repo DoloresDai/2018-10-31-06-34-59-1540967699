11c62b290da16d2c1960e542dc1e05be2ffb02ad
DoloresDai <841856753@qq.com>
2018-12-28 09:32:25 +0800

    Modify code by ES6

--- main/collectionOperator/collect_all_even.js
+++ main/collectionOperator/collect_all_even.js
@@ -1,12 +1,6 @@
 'use strict';
 
 function collect_all_even(collection) {
-  var evenCollection = new Array;
-  for (let i = 0; i < collection.length; i++) {
-    if (collection[i] % 2 === 0) {
-      evenCollection.push(collection[i]);
-    }
-  }
-  return evenCollection;
+  return collection.filter(x => x % 2 === 0);
 }
 module.exports = collect_all_even;
