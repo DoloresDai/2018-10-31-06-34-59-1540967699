f0d75721f18a8e989314f8e771e84d2b19fc59b7
DoloresDai <841856753@qq.com>
2018-12-31 12:18:53 +0800

    Modify code by ES6

--- main/section_3/practice_2/practice.js
+++ main/section_3/practice_2/practice.js
@@ -1,9 +1,7 @@
 function create_updated_collection(collection_a, object_b) {
-  for (let j = 0; j < collection_a.length; j++) {
-    if (object_b.value.includes(collection_a[j].key)) {
-      collection_a[j].count = collection_a[j].count - Math.floor(collection_a[j].count / 3);
-    }
-  }
+  collection_a.map(element => {
+    object_b.value.includes(element.key) ? element.count -= Math.floor(element.count / 3) : element.count;
+  });
   return collection_a;
 }
 module.exports = create_updated_collection;
