95efa692611e043403125cd46c95d8361b4ac9d1
DoloresDai <841856753@qq.com>
2018-12-31 12:02:10 +0800

    Modify code by ES6

--- main/section_2/practice_3/practice.js
+++ main/section_2/practice_3/practice.js
@@ -1,5 +1,23 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  return getObj(groupObj(collection));
 }
 
+function groupObj(collection) {
+  return collection.reduce((pre, value) => {
+    let letter = value.match(/[a-zA-Z]+/g)[0];
+    let number = value.match(/\d+/g);
+    let count = number ? number : 1;
+    pre[letter] = (pre[letter] + parseInt(count)) || parseInt(count);
+    return pre;
+  }, {});
+}
+
+function getObj(obj) {
+  return Object.keys(obj).map((value) => {
+    return {
+      name: value,
+      summary: obj[value]
+    };
+  });
+}
 module.exports = count_same_elements;
