cc841479594bdca5f4fca24618fc16ee716c90cf
DoloresDai <841856753@qq.com>
2018-12-31 12:57:51 +0800

    MOdify code for passing the test

--- main/filter/two_collections_practice_3.js
+++ main/filter/two_collections_practice_3.js
@@ -3,8 +3,11 @@
 function choose_divisible_integer(collection_a, collection_b) {
   return collection_a.filter(x => {
     for (let element of collection_b) {
-      x % element === 0;
+      if (x % element === 0) {
+        return true;
+      }
     }
-  })
+    return false;
+  });
 }
 module.exports = choose_divisible_integer;
