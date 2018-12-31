e9cfff78e15f815556ca332066edf34a96c55e9a
DoloresDai <841856753@qq.com>
2018-12-31 12:32:43 +0800

    Modify code by ES6

--- main/section_3/practice_3/practice.js
+++ main/section_3/practice_3/practice.js
@@ -1,43 +1,28 @@
-function groupingCountObj(key, val) {
-  var groupingCount = {
-    'key': key,
-    'count': val
-  };
-  return groupingCount;
+function count_same_elements(collection) {
+  return getObj(groupObj(collection));
 }
 
-function count_same_elements(collection) {
-  var count = 0;
-  var counts = [];
-  var noRepeatArray = [];
-  var groupingArray = [];
-  for (let i = 0; i < collection.length; i++) {
-    if (!noRepeatArray.includes(collection[i])) {
-      noRepeatArray.push(collection[i]);
-    }
-  }
-  for (let j = 0; j < noRepeatArray.length; j++) {
-    for (let k = 0; k < collection.length; k++) {
-      if (noRepeatArray[j] === (collection[k])) {
-        count++;
-      }
-    }
-    counts.push(count);
-    count = 0;
-  }
-  for (let index in noRepeatArray) {
-    groupingArray.push(groupingCountObj(noRepeatArray[index], counts[index]));
-  }
-  return groupingArray;
+function groupObj(collection) {
+  return collection.reduce((pre, value) => {
+    pre[value] = (pre[value] + 1) || 1;
+    return pre;
+  }, {});
+}
+
+function getObj(obj) {
+  return Object.keys(obj).map((value) => {
+    return {
+      key: value,
+      count: obj[value]
+    };
+  });
 }
 
 function create_updated_collection(collection_a, object_b) {
   collection_a = count_same_elements(collection_a);
-  for (let j = 0; j < collection_a.length; j++) {
-    if (object_b.value.includes(collection_a[j].key)) {
-      collection_a[j].count = collection_a[j].count - Math.floor(collection_a[j].count / 3);
-    }
-  }
+  collection_a.map(element => {
+    object_b.value.includes(element.key) ? element.count -= Math.floor(element.count / 3) : element.count;
+  });
   return collection_a;
 }
 module.exports = create_updated_collection;
--- main/section_3/practice_4/practice.js
+++ main/section_3/practice_4/practice.js
@@ -1,5 +1,31 @@
-function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+function convertNumberToLetter(collection) {
+  return getObj(groupObj(collection));
 }
 
+function groupObj(collection) {
+  return collection.reduce((pre, value) => {
+    let letter = value.match(/[a-zA-Z]+/g)[0];
+    let number = value.match(/\d+/g);
+    let count = number ? number : 1;
+    pre[letter] = (pre[letter] + parseInt(count)) || parseInt(count);
+    return pre;
+  }, {});
+}
+
+function getObj(obj) {
+  return Object.keys(obj).map((value) => {
+    return {
+      key: value,
+      count: obj[value]
+    };
+  });
+}
+
+function create_updated_collection(collection_a, object_b) {
+  collection_a = convertNumberToLetter(collection_a);
+  collection_a.map(element => {
+    object_b.value.includes(element.key) ? element.count -= Math.floor(element.count / 3) : element.count;
+  });
+  return collection_a;
+}
 module.exports = create_updated_collection;
