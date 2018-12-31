7d9025b9acc7dfed2dd4f9eb30317efc1719b7d2
DoloresDai <841856753@qq.com>
2018-12-31 12:00:12 +0800

    Modify code by ES6

--- main/section_2/practice_2/practice.js
+++ main/section_2/practice_2/practice.js
@@ -1,45 +1,23 @@
-function groupingCountObj(key, val) {
-  var groupingCount = {
-    'key': key,
-    'count': val
-  };
-  return groupingCount;
-}
-
 function convertNumberToLetter(collection) {
-  for (let n = 0; n < collection.length; n++) {
-    if (/[0-9]/.test(collection[n])) {
-      for (let d = 0; d < collection[n].replace(/[^a-z]/, "").replace(/[a-z]/, ""); d++) {
-        collection.splice(n + d, 0, collection[n].match(/[a-z]/g)[0]);
-      }
-    }
-  }
-  return collection;
+  return getObj(groupObj(collection));
 }
 
-function count_same_elements(collection) {
-  var count = 0;
-  var counts = [];
-  var noRepeatArray = [];
-  var groupingArray = [];
-  collection = convertNumberToLetter(collection);
-  for (let i = 0; i < collection.length; i++) {
-    if (!noRepeatArray.includes(collection[i])) {
-      noRepeatArray.push(collection[i].match(/[a-z]/ig)[0]);
-    }
-  }
-  for (let j = 0; j < noRepeatArray.length; j++) {
-    for (let k = 0; k < collection.length; k++) {
-      if (noRepeatArray[j] === (collection[k])) {
-        count++;
-      }
-    }
-    counts.push(count);
-    count = 0;
-  }
-  for (let index in noRepeatArray) {
-    groupingArray.push(groupingCountObj(noRepeatArray[index], counts[index]));
-  }
-  return groupingArray;
+function groupObj(collection) {
+  return collection.reduce((pre, value) => {
+    let letter = value.match(/[a-zA-Z]+/g)[0];
+    let number = value.match(/\d+/g);
+    let count = number ? number : 1;
+    pre[letter] = (pre[letter] + parseInt(count)) || parseInt(count);
+    return pre;
+  }, {});
+}
+
+function getObj(obj) {
+  return Object.keys(obj).map((value) => {
+    return {
+      key: value,
+      count: obj[value]
+    };
+  });
 }
 module.exports = convertNumberToLetter;
