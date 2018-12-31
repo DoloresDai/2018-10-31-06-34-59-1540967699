68a8045ad78d07d5ccfc6b3041b16d9f77ea58ee
DoloresDai <841856753@qq.com>
2018-12-31 12:59:35 +0800

    MOdify code for passing the test

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,6 +1,6 @@
 'use strict';
 var calculate_average = function(collection) {
   let oddIndexArray = collection.filter((value, index) => index % 2 === 1);
-  return evenIndexArray.reduce((x, y) => x + y) / evenIndexArray.length;
+  return oddIndexArray.reduce((x, y) => x + y) / oddIndexArray.length;
 }
 module.exports = calculate_average;
